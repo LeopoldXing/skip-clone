e7043ad9aca88476eece049270a0abe56ec0c979
LeopoldHsing <LeopoldHsing@gmail.com>
2024-09-21 03:03:06 -0500

    feat: add api: get current user's orders

--- src/api/OrderApi.ts
+++ src/api/OrderApi.ts
@@ -1,6 +1,7 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import { toast } from "sonner";
+import { Order } from "@/types.ts";
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -19,6 +20,36 @@ type CheckoutSessionRequest = {
   restaurantId: string;
 };
 
+/**
+ * get current user's orders
+ */
+const useGetMyOrders = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const getMyOrdersRequest = async (): Promise<Order[]> => {
+    const accessToken = await getAccessTokenSilently();
+    const response = await fetch(`${BASE_URL}/api/order`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    });
+    if (!response.ok) {
+      throw new Error("Failed to get order list");
+    }
+    return response.json();
+  }
+
+  const { data: myOrders, isLoading } = useQuery(
+      'fetchMyOrders',
+      getMyOrdersRequest
+  );
+
+  return { myOrders, isLoading };
+}
+
+/**
+ * custom hook to create checkout session
+ */
 const useCreateCheckoutSession = () => {
   const { getAccessTokenSilently } = useAuth0();
 
@@ -48,4 +79,4 @@ const useCreateCheckoutSession = () => {
   return { createCheckoutSession, isLoading };
 }
 
-export { useCreateCheckoutSession };
+export { useCreateCheckoutSession, useGetMyOrders };
